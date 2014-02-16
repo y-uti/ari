9078a148ae6c2881d26378afff2378342a90480d
y-uti <y-uti@users.noreply.github.com>
2014-02-16 19:00:06 +0900

    実装を整理

--- js/ari.js
+++ js/ari.js
@@ -1,76 +1,41 @@
-
 function calcRandIndex(c1, c2) {
-    var table = buildContingencyTable(c1, c2);
-    var n = combination2(c1.length);
-    var oo = 0;
-    for (var r = 0; r < table.length; r++) {
-	for (var c = 0; c < table[r].length; c++) {
-	    oo += combination2(table[r][c]);
-	}
-    }
-    var c1c = countValues(c1).map(combination2);
-    var ox = -oo;
-    for (var i = 0; i < c1c.length; i++) {
-	ox += c1c[i];
-    }
-    var xo = -oo;
-    var c2c = countValues(c2).map(combination2);
-    for (var i = 0; i < c2c.length; i++) {
-	xo += c2c[i];
-    }
-    var xx = n - oo - ox - xo;
-
-    return (oo + xx) / n;
+    return calculate(c1, c2, function (n, oo, c1o, c2o) {
+	return (n + 2 * oo - c1o - c2o) / n;
+    });
 }
 
 function calcAdjustedRandIndex(c1, c2) {
+    return calculate(c1, c2, function (n, oo, c1o, c2o) {
+	return (oo * n - c1o * c2o) / ((c1o + c2o) * n / 2 - c1o * c2o);
+    });
+}
+
+function calculate(c1, c2, f) {
     var table = buildContingencyTable(c1, c2);
     var n = combination2(c1.length);
-    var oo = 0;
-    for (var r = 0; r < table.length; r++) {
-	for (var c = 0; c < table[r].length; c++) {
-	    oo += combination2(table[r][c]);
-	}
-    }
-    var c1o = 0;
-    var c1c = countValues(c1).map(combination2);
-    for (var i = 0; i < c1c.length; i++) {
-	c1o += c1c[i];
-    }
-    var c2o = 0;
-    var c2c = countValues(c2).map(combination2);
-    for (var i = 0; i < c2c.length; i++) {
-	c2o += c2c[i];
-    }
+    var oo = d3.sum(d3.merge(table).map(combination2));
+    var c1o = d3.sum(countValues(c1).map(combination2));
+    var c2o = d3.sum(countValues(c2).map(combination2));
 
-    return (oo * n - c1o * c2o) / ((c1o + c2o) * n / 2 - c1o * c2o);
+    return f(n, oo, c1o, c2o);
 }
 
 function buildContingencyTable(c1, c2) {
-    var rows = Math.max.apply(null, c1) + 1;
-    var cols = Math.max.apply(null, c2) + 1;
-    var table = new Array(rows);
-    for (var r = 0; r < table.length; r++) {
-	table[r] = new Array(cols);
-	for (var c = 0; c < table[r].length; c++) {
-	    table[r][c] = 0;
-	}
-    }
-    for (var d = 0; d < c1.length; d++) {
-	table[c1[d]][c2[d]]++;
-    }
+    var table = buildMatrix(d3.max(c1) + 1, d3.max(c2) + 1);
+    d3.zip(c1, c2).forEach(function (v) { table[v[0]][v[1]]++; });
+
     return table;
 }
 
+function buildMatrix(r, c) {
+    return d3.range(r).map(function () { return d3.range(c).map(function () { return 0; }); });
+}
+
 function countValues(c) {
-    var table = new Array(Math.max.apply(null, c) + 1);
-    for (var i = 0; i < table.length; i++) {
-	table[i] = 0;
-    }
-    for (var i = 0; i < c.length; i++) {
-	table[c[i]]++;
-    }
-    return table;
+    var result = d3.range(d3.max(c) + 1).map(function () { return 0; });
+    c.forEach(function (i) { result[i]++; });
+
+    return result;
 }
 
 function combination2(n) {
